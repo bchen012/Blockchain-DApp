c819d7b4ff86dd7b0c2234bd50db4c59fbe648cc
Benjamin Chen <ftt.benjamin.chen@grabtaxi.com>
2021-11-14 00:36:03 +0800

    moved pool info to liquidate page

--- Frontend/packages/app/src/components/adminPage/InitiatePage.tsx
+++ Frontend/packages/app/src/components/adminPage/InitiatePage.tsx
@@ -1,17 +1,12 @@
 import { TextField, Card, CardMedia, CardContent, Button} from '@material-ui/core';
-import React, { useState } from 'react';
-import {K_REWARD_CONTRACT_ADDRESS, YM1_CONTRACT_ADDRESS, YM2_CONTRACT_ADDRESS} from "../../config";
+import React from 'react';
 
 type InitiatePageProps = {
     init: () => void,
     initialStake: () => void,
-    tokenA_P1: string,
-    tokenB_P1: string,
-    tokenA_P2: string,
-    tokenB_P2: string,
 }
 
-export const InitiatePage = ({init, initialStake, tokenA_P1, tokenB_P1, tokenA_P2, tokenB_P2}: InitiatePageProps) => {
+export const InitiatePage = ({init, initialStake}: InitiatePageProps) => {
 
 
     return (
@@ -23,17 +18,6 @@ export const InitiatePage = ({init, initialStake, tokenA_P1, tokenB_P1, tokenA_P
                 alt="green iguana"
             />
             <CardContent>
-                <div>
-                    <h2>Pool 1 info</h2>
-                    <h4>Token A amount: {tokenA_P1}</h4>
-                    <h4>Token B amount: {tokenB_P1}</h4>
-                    <br/>
-                    <h2>Pool 2 info</h2>
-                    <h4>Token A amount: {tokenA_P2}</h4>
-                    <h4>Token B amount: {tokenB_P2}</h4>
-                    <br/>
-                </div>
-                <br/>
                 <Button variant="contained" onClick={() => {init()}}>Initiate App</Button>
                 <br/>
                 <br/>
--- Frontend/packages/app/src/components/liquidatePage/LiquidatePage.tsx
+++ Frontend/packages/app/src/components/liquidatePage/LiquidatePage.tsx
@@ -1,7 +1,16 @@
 import LiquidateLayout from "./LiquidateLayout";
 import {Content} from '@backstage/core-components';
 import Web3 from "web3";
-import { KV2_CONTRACT_ADDRESS, KV2_ABI } from "../../config";
+import {
+    KV2_CONTRACT_ADDRESS,
+    KV2_ABI,
+    K_MINE_ABI,
+    K_MINE_CONTRACT_ADDRESS,
+    K_REWARD_CONTRACT_ADDRESS,
+    YM1_CONTRACT_ADDRESS,
+    YM2_CONTRACT_ADDRESS
+} from "../../config";
+
 import {Button, Card, CardContent, CardMedia, TextField} from "@material-ui/core";
 import React, {useState} from "react";
 
@@ -11,17 +20,38 @@ export const LiquidatePage = () => {
     const [exchangeRate, setExchangeRate] = useState<string>('');
     const [account, setAccount] = useState<any>('');
 
+    const [tokenA_P2, setTokenA_P2] = useState<string>('0');
+    const [tokenB_P2, setTokenB_P2] = useState<string>('0');
+
+    const [tokenA_P1, setTokenA_P1] = useState<string>('0');
+    const [tokenB_P1, setTokenB_P1] = useState<string>('0');
+
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
     const contract = new web3.eth.Contract(KV2_ABI, KV2_CONTRACT_ADDRESS);
+    const k_mine_contract = new web3.eth.Contract(K_MINE_ABI, K_MINE_CONTRACT_ADDRESS);
+
     web3.eth.getAccounts().then(accounts => {
         setAccount(accounts[0]);
     });
 
-    const getExchangeRate = async () => {
-        await contract.methods.getExchangeRate().call().then(exr => {
-            setExchangeRate(exr);
+    web3.eth.getBalance(K_MINE_CONTRACT_ADDRESS).then(result => {
+        console.log('Klee_mine Eth Balance:', result/1e18)
+    })
+
+    const getPoolInfo = async () => {
+        await k_mine_contract.methods.getPoolInfo(0).call().then(Result => {
+            console.log('Pool 0 info:', Result);
+
+            setTokenA_P1(Result[0]/1e18);
+            setTokenB_P1(Result[1]/1e18);
         });
-    }
+        await k_mine_contract.methods.getPoolInfo('1').call().then(Result => {
+            setTokenA_P2(Result[0]/1e18)
+            setTokenB_P2(Result[1]/1e18)
+        });
+    };
+
+    getPoolInfo().then();
 
     const handleAmountChange = (e) => {
         setEtherAmount(e.target.value);
@@ -36,8 +66,6 @@ export const LiquidatePage = () => {
             });
     }
 
-    getExchangeRate().then();
-
     return (
         <LiquidateLayout >
             <Content>
@@ -49,26 +77,16 @@ export const LiquidatePage = () => {
                         alt="green iguana"
                     />
                     <CardContent>
-                        <h2>Enter the amount of Ether you want to swap to KV2:</h2>
                         <div>
-                            <TextField
-                                required
-                                id="outlined-required"
-                                label="Amount"
-                                defaultValue=""
-                                value={etherAmount}
-                                fullWidth
-                                onChange={handleAmountChange}
-                                autoComplete={"off"}
-                            />
+                            <h2>Pool 1 info</h2>
+                            <h4>Token A amount: {tokenA_P1}</h4>
+                            <h4>Token B amount: {tokenB_P1}</h4>
                             <br/>
-                            <h2>To be received: </h2>
-                            <h3>
-                                {buyAmount} KV2
-                            </h3>
-
+                            <h2>Pool 2 info</h2>
+                            <h4>Token A amount: {tokenA_P2}</h4>
+                            <h4>Token B amount: {tokenB_P2}</h4>
+                            <br/>
                         </div>
-                        <br/>
                         <Button variant="contained" onClick={()=>{
                             buyCoin().then(()=>{setEtherAmount('')})
                         }}>Buy</Button>
