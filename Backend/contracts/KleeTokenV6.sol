488d4851274e088359313c400e050fb1f236f1f4
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2021-11-16 21:02:26 +0800

    feat: migrate price to backend responsibility
    priceRange is an array for price per rarity level
    0 -- common
    1 -- legendary
    2 -- mythic
    provide getter and setter for price range.
    modify awardNFT to be public and add the requirement based on rarity

--- Backend/contracts/KleeTokenV6.sol
+++ Backend/contracts/KleeTokenV6.sol
@@ -20,8 +20,9 @@ contract KleeTokenV6 is ERC721URIStorage, Ownable {
     using SafeCast for uint256;
 
 
-    uint256 tokenCounter;
-    mapping (uint256 => address) tokenToOwner;
+    uint256 public tokenCounter;
+    mapping (uint256 => uint256) public priceRange;
+    uint256 public existingPrice;
 
     IERC20 private _RewardToken;
     address _RewardTokenAddress = address(0x97Fd477e1893a2eEDcC7DEFE19fcDA7bF3EB6F1f);
@@ -29,19 +30,39 @@ contract KleeTokenV6 is ERC721URIStorage, Ownable {
     constructor() ERC721("KleeTokenV6", "KT6") {
         _RewardToken = IERC20(_RewardTokenAddress);
         tokenCounter = 0;
+        existingPrice = 3;
+        priceRange[0] = 500 * (10 **18);  //common
+        priceRange[1] = 1000 * (10 ** 18);  //legendary
+        priceRange[2] = 10000 * (10 ** 18);  //mythical
+    }
+
+    function modifyPriceRange(uint256 id,uint256 price) public onlyOwner {
+        priceRange[id] = price;
+    }
+
+    function addPrice(uint256 id,uint256 price) public onlyOwner {
+        require(id >= existingPrice);
+        priceRange[id] = price; 
+    }
+
+    function getPriceRange() public view returns (address  [] memory) {
+        return priceRange;
     }
 
     //@dev: when the LP has harvested enough reward token to exchange for an nft
     //    : this function will be called by owner, with the amount of token needed for the nft set by front-end
     //    : for e.g if legendary tier NFT is to be minted, amount should be large
     //    : backend doesnt do that stuff
-    function awardNFT(string memory tokenURI,uint256 amount,address rewardeeAddress) public onlyOwner {
+    function awardNFT(string memory tokenURI,uint256 rarity, uint256 amount,address rewardeeAddress) public  {
         //our id starts from 1 cuz matlab
+        require(amount >= priceRange[rarity]);
         tokenCounter = tokenCounter.add(1);
         _safeMint(_msgSender(), tokenCounter);
         _setTokenURI(tokenCounter, tokenURI);
         _RewardToken.transferFrom(rewardeeAddress,address(this),amount);
     }
+
+
 
 
 
